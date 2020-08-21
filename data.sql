ed3ce9ff369aafef352de366ea0f2474c7bc25e5
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-21 19:42:39 -0400

    added initial db data

---
+++ data.sql
@@ -0,0 +1,13 @@
+insert into cursovalues (1, 'Primero básico');
+insert into cursovalues (2, 'Segundo básico');
+insert into cursovalues (3, 'Tercero básico');
+
+insert into alumnovalues (1111, 1, 'Alonso Aravena');
+insert into alumnovalues (2222, 1, 'Bernardita Barraza');
+insert into alumnovalues (3333, 1, 'Camilo Cordero');
+insert into alumnovalues (4444, 1, 'Daniela Diaz');
+insert into alumnovalues (5555, 2, 'Eugenia Echeverría');
+insert into alumnovalues (6666, 2, 'Felipe Fuentes');
+insert into alumnovalues (7777, 2, 'Gabriela Gómez');
+insert into alumnovalues (8888, 3, 'HéctorHorta');
+insert into alumnovalues (9999, 3, 'IvánIsla');
