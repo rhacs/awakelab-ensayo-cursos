606f624d29b54d775a77d8fd4e7fcb26be752929
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-21 19:43:32 -0400

    fixed typo

--- data.sql
+++ data.sql
@@ -1,13 +1,13 @@
-insert into cursovalues (1, 'Primero básico');
-insert into cursovalues (2, 'Segundo básico');
-insert into cursovalues (3, 'Tercero básico');
+insert into curso values (1, 'Primero básico');
+insert into curso values (2, 'Segundo básico');
+insert into curso values (3, 'Tercero básico');
 
-insert into alumnovalues (1111, 1, 'Alonso Aravena');
-insert into alumnovalues (2222, 1, 'Bernardita Barraza');
-insert into alumnovalues (3333, 1, 'Camilo Cordero');
-insert into alumnovalues (4444, 1, 'Daniela Diaz');
-insert into alumnovalues (5555, 2, 'Eugenia Echeverría');
-insert into alumnovalues (6666, 2, 'Felipe Fuentes');
-insert into alumnovalues (7777, 2, 'Gabriela Gómez');
-insert into alumnovalues (8888, 3, 'HéctorHorta');
-insert into alumnovalues (9999, 3, 'IvánIsla');
+insert into alumno values (1111, 1, 'Alonso Aravena');
+insert into alumno values (2222, 1, 'Bernardita Barraza');
+insert into alumno values (3333, 1, 'Camilo Cordero');
+insert into alumno values (4444, 1, 'Daniela Diaz');
+insert into alumno values (5555, 2, 'Eugenia Echeverría');
+insert into alumno values (6666, 2, 'Felipe Fuentes');
+insert into alumno values (7777, 2, 'Gabriela Gómez');
+insert into alumno values (8888, 3, 'HéctorHorta');
+insert into alumno values (9999, 3, 'IvánIsla');
