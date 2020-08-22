8fb9346ce75ba46746c5330046c3c185b4b460b8
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-21 21:12:59 -0400

    modified the path to accept an optional Curso id

--- src/main/java/io/github/rhacs/cursos/controladores/HomeController.java
+++ src/main/java/io/github/rhacs/cursos/controladores/HomeController.java
@@ -1,17 +1,24 @@
 package io.github.rhacs.cursos.controladores;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
+import io.github.rhacs.cursos.modelos.Alumno;
 import io.github.rhacs.cursos.modelos.Curso;
 import io.github.rhacs.cursos.repositorios.CursosRepositorio;
 
@@ -43,15 +50,39 @@ public class HomeController {
     /**
      * Muestra el listado de cursos disponibles en el repositorio
      * 
+     * @param id      identificador numérico del {@link Curso}
      * @param request objeto {@link HttpServletRequest} que contiene la información
      *                de la solicitud que le envía el cliente al servlet
      * @param modelo  objeto {@link Model} que contiene el modelo de la vista
      * @return un objeto {@link String} con el nombre de la vista
      */
-    @GetMapping
-    public String listarCursos(HttpServletRequest request, Model modelo) {
+    @GetMapping(path = { "/", "/{id:^[0-9]+$}" })
+    public String listarCursos(@PathVariable Optional<Long> id, HttpServletRequest request, Model modelo) {
         // Depuración
         logger.info("[WEB] Solicitud {}: {}", request.getMethod(), request.getRequestURL());
+
+        // Verificar si el id está presente en la url
+        if (id.isPresent()) {
+            // Inicializar el Rest Template
+            RestTemplate rest = new RestTemplate();
+
+            // Efectuar petición GET a la API
+            ResponseEntity<Alumno[]> respuesta = rest
+                    .getForEntity("http://localhost/cursos/api/alumnos/curso/" + id.get(), Alumno[].class);
+
+            // Verificar estado de la respuesta
+            if (respuesta.getStatusCode().equals(HttpStatus.OK)) {
+                // Extraer objetos
+                List<Alumno> alumnos = Arrays.asList(respuesta.getBody());
+
+                // Agregar listado al modelo
+                modelo.addAttribute("alumnos", alumnos);
+            } else {
+                // Agregar error al modelo
+                modelo.addAttribute("error",
+                        "No existe el Curso con el identificador numérico <strong>" + id.get() + "</strong>.");
+            }
+        }
 
         // Buscar listado de cursos
         List<Curso> cursos = cursosRepositorio.findAll();
